fae2593401017472e2e8762b2b3df669f4e02ac0
Mark R Ohnsman <MarkOhnsman@users.noreply.github.com>
2020-11-18 16:25:13 -0700

    Update arrays.js

--- app/arrays.js
+++ app/arrays.js
@@ -34,21 +34,8 @@ function elemsTimesLength(arr) {
 // ------------------------------------------
 
 
-// 4. Given a multidimensional array, return a single dimension array with all of the primitive data elements within the multidimensional array.
-// Example:
-// input:  [[['legume'], 3, []], 2, ['tree', [{}, [5]]]]
-// output: ['legume', 3, 2, 'tree', 5]
-// Primitive data types - https://developer.mozilla.org/en-US/docs/Glossary/Primitive
 
-function arrayFlattener(arr) {
-
-}
-
-
-// ------------------------------------------
-
-
-//5. Flights from Boise. Write a function that will use the following data to return the cost of flights from boise to a neighboring city, by default return the standard cost unless firstClass is set to true
+//4. Flights from Boise. Write a function that will use the following data to return the cost of flights from boise to a neighboring city, by default return the standard cost unless firstClass is set to true
 
 let flights = [{
     from: 'BOI',
@@ -83,7 +70,7 @@ function flightCost(destination, firstClass) {
 // ------------------------------------------
 
 
-// 6. Given a number, return the corresponding user object from the staff array that has the given number as the value of their id property. If no user is found, return an object with an error property and value of "No user with that id."
+// 5. Given a number, return the corresponding user object from the staff array that has the given number as the value of their id property. If no user is found, return an object with an error property and value of "No user with that id."
 // Example:
 // input: 17
 // output: {id: 17, name: 'St. MaryLou de la Playa Carmen'}
@@ -104,7 +91,7 @@ function findById(id) {
 // ------------------------------------------
 
 
-// 7. Write a function that accepts a name argument and will loop over theBand members and return the band member's name and the instrument that he/she plays. Use string concatenation or interpolation to return a sentence with the following structure: "[band-members-name] is in the band and plays the [band-members-instrument]".
+// 6. Write a function that accepts a name argument and will loop over theBand members and return the band member's name and the instrument that he/she plays. Use string concatenation or interpolation to return a sentence with the following structure: "[band-members-name] is in the band and plays the [band-members-instrument]".
 // Example:
 // input: 'Johnny P'
 // output: "Johnny P is in the band and plays the sax"
